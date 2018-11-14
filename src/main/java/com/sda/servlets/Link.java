047d03a76d3f0c858b8b0ea03c0febe83a4e1187
Szymon Nowak <nowszy94@gmai.com>
2018-11-14 18:26:54 +0100

    links | refactor to new class

---
+++ src/main/java/com/sda/servlets/Link.java
@@ -0,0 +1,19 @@
+package com.sda.servlets;
+
+public class Link {
+    private String url;
+    private String text;
+
+    public Link(String url, String text) {
+        this.url = url;
+        this.text = text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
--- src/main/java/com/sda/servlets/LinksServlet.java
+++ src/main/java/com/sda/servlets/LinksServlet.java
@@ -6,22 +6,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinksServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
 
+        List<Link> links = new ArrayList<>();
+        links.add(new Link("http://www.google.com", "Google"));
+        links.add(new Link("http://pl.wikipedia.com", "Wikipedia"));
+        links.add(new Link("http://onet.pl", "Onet"));
+
         resp.addHeader("Content-Type", "text/html");
 
         writer.println("<div>");
         writer.println("Moje linki:");
-        writer.println("<br/>");
-        writer.println("<a href=\"http://www.google.com\">Google</a>");
-        writer.println("<br/>");
-        writer.println("<a href=\"http://pl.wikipedia.com\">Wikipedia</a>");
-        writer.println("<br/>");
-        writer.println("<a href=\"http://onet.pl\">Onet</a>");
+        for (Link link : links) {
+            writer.println("<br/>");
+            writer.println("<a href=\"" + link.getUrl() + "\">" + link.getText() + "</a>");
+        }
         writer.println("</div>");
+//        writer.println("<br/>");
+//        writer.println("<a href=\"http://www.google.com\">Google</a>");
+//        writer.println("<br/>");
+//        writer.println("<a href=\"http://pl.wikipedia.com\">Wikipedia</a>");
+//        writer.println("<br/>");
+//        writer.println("<a href=\"http://onet.pl\">Onet</a>");
     }
 }
