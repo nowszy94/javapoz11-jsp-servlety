817be910e22612b7219479eb1b6c8d8860934885
Szymon Nowak <nowszy94@gmai.com>
2018-11-14 19:47:11 +0100

    links | doPost and form

--- src/main/java/com/sda/servlets/LinksServlet.java
+++ src/main/java/com/sda/servlets/LinksServlet.java
@@ -10,29 +10,67 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LinksServlet extends HttpServlet {
+
+    private List<Link> links;
+
+    @Override
+    public void init() throws ServletException {
+        this.links = new ArrayList<>();
+
+        links.add(new Link("http://www.google.com", "Google"));
+        links.add(new Link("http://pl.wikipedia.com", "Wikipedia"));
+        links.add(new Link("http://onet.pl", "Onet"));
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
 
-        List<Link> links = new ArrayList<>();
-        links.add(new Link("http://www.google.com", "Google"));
-        links.add(new Link("http://pl.wikipedia.com", "Wikipedia"));
-        links.add(new Link("http://onet.pl", "Onet"));
-
         resp.addHeader("Content-Type", "text/html");
 
         writer.println("<div>");
+
+        String errorMessage = req.getParameter("error_message");
+        if (errorMessage != null) {
+            writer.println("<p style=\"color: red;\">" + errorMessage + "</p>");
+        }
+
+        createForm(writer);
+
         writer.println("Moje linki:");
         for (Link link : links) {
             writer.println("<br/>");
             writer.println("<a href=\"" + link.getUrl() + "\">" + link.getText() + "</a>");
         }
         writer.println("</div>");
-//        writer.println("<br/>");
-//        writer.println("<a href=\"http://www.google.com\">Google</a>");
-//        writer.println("<br/>");
-//        writer.println("<a href=\"http://pl.wikipedia.com\">Wikipedia</a>");
-//        writer.println("<br/>");
-//        writer.println("<a href=\"http://onet.pl\">Onet</a>");
+    }
+
+    private void createForm(PrintWriter writer) {
+        String form = "<form action=\"\" method=\"post\">\n" +
+                "    Link: <input type=\"text\" name=\"link\">\n" +
+                "    <br>\n" +
+                "    Text: <input type=\"text\" name=\"text\">\n" +
+                "    <br>\n" +
+                "    <input type=\"submit\">\n" +
+                "</form>";
+        writer.println(form);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String link = req.getParameter("link");
+        String text = req.getParameter("text");
+
+        StringBuilder redirectBuilder = new StringBuilder();
+        redirectBuilder.append(req.getContextPath())
+                .append(req.getServletPath());
+
+        if (link == null || text == null) {
+            redirectBuilder.append("?error_message=Bledne dane");
+        } else {
+            Link linkObject = new Link(link, text);
+            links.add(linkObject);
+        }
+        resp.sendRedirect(redirectBuilder.toString());
     }
 }
